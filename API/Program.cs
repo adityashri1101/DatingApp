f1399aabf73ca96db035f814b57d01f9f0ed8296
Aditya Shrivastava <adityashri1101@gmail.com>
2023-05-15 23:56:44 +0530

    Angular skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +29,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
